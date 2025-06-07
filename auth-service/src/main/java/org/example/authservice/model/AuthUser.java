7768900b671564d13c637b6bd9b1324a8e9fb444
omarb <omarb2515@gmail.com>
2025-06-07 10:48:18 +0300

    Refactor AuthUser and ROLE models for user authentication.

--- auth-service/src/main/java/org/example/authservice/model/AuthUser.java
+++ auth-service/src/main/java/org/example/authservice/model/AuthUser.java
@@ -1,27 +1,67 @@
 package org.example.authservice.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import org.example.userservice.model.User;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
+@Builder
 @Data
 @Entity
 @Table(name = "authUsers")
-public class AuthUser {
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthUser implements UserDetails {
     @Id
-    private UUID userId;
+    private UUID id;
     @Column(unique = true, nullable = false)
     private String username;
+    @Column(unique = true)
+    private String email;
     @Column(nullable = false)
     private String password;
     private ROLE role;
-    private Set<UUID> refreshTokens;
-    @OneToOne
-    @MapsId
-    @JoinColumn(name = "userId")
-    private User user;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<RefreshToken> refreshTokens = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
 }
 
--- auth-service/src/main/java/org/example/authservice/model/ROLE.java
+++ auth-service/src/main/java/org/example/authservice/model/ROLE.java
@@ -2,5 +2,7 @@ package org.example.authservice.model;
 
 public enum ROLE {
     USER,
+    ORGANIZER,
     ADMIN
 }
+
