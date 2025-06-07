8ec7ea051a3e69a65a64651b8efef990c996b159
omarb <omarb2515@gmail.com>
2025-06-07 10:46:04 +0300

    Add DTO classes for authentication-related operations

---
+++ auth-service/src/main/java/org/example/authservice/dto/CustomUserDetails.java
@@ -0,0 +1,65 @@
+package org.example.authservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Set;
+import java.util.UUID;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Builder
+@Getter
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private String username;
+
+    private UUID id;
+
+    @JsonIgnore
+    private String password;
+
+    @JsonDeserialize(contentAs = CustomGrantedAuthority.class)
+    private Set<GrantedAuthority> authorities;
+
+    private final boolean accountNonExpired;
+    private final boolean accountNonLocked;
+    private final boolean credentialsNonExpired;
+    private final boolean enabled;
+
+    public void eraseCredentials() {
+        this.password = null;
+    }
+
+    public int hashCode() {
+        return this.username.hashCode();
+    }
+
+    public boolean equals(final Object obj) {
+        if (obj instanceof CustomUserDetails) {
+            return this.username
+                    .equals(((CustomUserDetails) obj).getUsername());
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return this.getClass().getName() + " ["
+                + "Username=" + this.username + ", "
+                + "Password=[PROTECTED], "
+                + "Enabled=" + this.enabled + ", "
+                + "AccountNonExpired=" + this.accountNonExpired + ", "
+                + "CredentialsNonExpired=" + this.credentialsNonExpired + ", "
+                + "AccountNonLocked=" + this.accountNonLocked + ", "
+                + "Granted Authorities=" + this.authorities + "]";
+    }
+}
