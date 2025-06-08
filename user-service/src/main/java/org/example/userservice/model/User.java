7b2fc6eeabf12eba81a1361be22a7dab95cd6288
OmarMUhammed03 <omarb2515@gmail.com>
2025-06-08 20:39:58 +0300

    Refactor User entity to improve field naming and add validation method

--- user-service/src/main/java/org/example/userservice/model/User.java
+++ user-service/src/main/java/org/example/userservice/model/User.java
@@ -2,13 +2,8 @@ package org.example.userservice.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import org.example.authservice.model.AuthUser;
-import org.example.bookingservice.model.Booking;
-import org.example.eventservice.model.Event;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -16,18 +11,23 @@ import java.util.UUID;
 @Table(name = "users")
 public class User {
     @Id
-    private UUID userId;
+    private UUID id;
     @Column(unique = true)
     private String email;
     private String firstname;
     private String lastname;
     private LocalDate dateOfBirth;
     private String gender;
-    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private AuthUser authUser;
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Booking> bookings = new ArrayList<>();
-    @OneToMany(mappedBy = "creator")
-    private List<Event> createdEvents = new ArrayList<>();
+    private String profilePictureUrl;
+
+    public boolean isValid() {
+        return id != null &&
+                email != null && !email.isBlank()
+                && firstname != null && !firstname.isBlank()
+                && lastname != null && !lastname.isBlank()
+                && dateOfBirth != null
+                && gender != null && !gender.isBlank();
+    }
+
 }
 
