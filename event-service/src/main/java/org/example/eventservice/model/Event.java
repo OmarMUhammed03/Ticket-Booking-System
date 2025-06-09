0876f7325820737ec00dac4fb95ab0610a0219dc
OmarMUhammed03 <omarb2515@gmail.com>
2025-06-09 15:31:02 +0300

    Add EventRepository, enhance Event model with associations

--- event-service/src/main/java/org/example/eventservice/model/Event.java
+++ event-service/src/main/java/org/example/eventservice/model/Event.java
@@ -1,9 +1,12 @@
 package org.example.eventservice.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -11,16 +14,27 @@ import java.util.UUID;
 @Table(name = "events")
 public class Event {
     @Id
-    private UUID eventId;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
     @Column(nullable = false)
     private String name;
     @Column(nullable = false)
-    private String location;
+    private LocalDateTime startDate;
     @Column(nullable = false)
-    private LocalDateTime date;
+    private LocalDateTime endDate;
     @Lob
     private String description;
     @Column(nullable = false)
     private UUID creatorId;
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Ticket> tickets = new ArrayList<>();
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "venue_id", nullable = false)
+    private Venue venue;
+
+    public void addTicket(Ticket ticket) {
+        tickets.add(ticket);
+    }
 }
 
