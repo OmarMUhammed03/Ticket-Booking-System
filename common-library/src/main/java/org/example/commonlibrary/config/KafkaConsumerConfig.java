f1a1211623f2c53eee98c7391c60edb542adf80b
OmarMUhammed03 <omarb2515@gmail.com>
2025-06-12 15:37:47 +0300

    Refactored Kafka configurations into common-library to improve reusability across services.

--- common-library/src/main/java/org/example/commonlibrary/config/KafkaConsumerConfig.java
+++ common-library/src/main/java/org/example/commonlibrary/config/KafkaConsumerConfig.java
@@ -1,8 +1,8 @@
-package org.example.eventservice.config;
+package org.example.commonlibrary.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
