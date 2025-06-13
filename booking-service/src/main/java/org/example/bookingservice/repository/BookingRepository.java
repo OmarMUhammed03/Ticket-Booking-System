73aa3419c9aae11376633132eb1618a4d60e1e56
OmarMUhammed03 <omarb2515@gmail.com>
2025-06-13 03:46:47 +0300

    Add BookingRepository interface with method to fetch bookings by user ID

---
+++ booking-service/src/main/java/org/example/bookingservice/repository/BookingRepository.java
@@ -0,0 +1,13 @@
+package org.example.bookingservice.repository;
+
+import org.example.bookingservice.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, UUID> {
+    List<Booking> findByUserId(UUID userId);
+}
