dc60d70582d48d352f6e01dcf4976e1c066f1916
OmarMUhammed03 <omarb2515@gmail.com>
2025-06-13 03:47:15 +0300

    Update `MessageConsumer` to handle ticket reservation flow and integrate with `EventService` and `MessageProducer`

--- event-service/src/main/java/org/example/eventservice/kafka/MessageConsumer.java
+++ event-service/src/main/java/org/example/eventservice/kafka/MessageConsumer.java
@@ -1,17 +1,42 @@
 package org.example.eventservice.kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.example.commonlibrary.kafka.MessageProducer;
+import org.example.eventservice.service.EventService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RequiredArgsConstructor
 @Component
 public class MessageConsumer {
     private final Logger LOGGER = LoggerFactory.getLogger(MessageConsumer.class);
+    private final EventService eventService;
+    private final MessageProducer messageProducer;
 
-    @KafkaListener(topics = "ticket-reserved", groupId = "${spring.kafka.consumer.group-id}")
-    public void listen(Object message) {
+    @KafkaListener(topics = "reserve-ticket", groupId = "${spring.kafka.consumer.group-id}")
+    public void reserveTicket(Object message) throws JsonProcessingException {
         LOGGER.info("Received message='{}'", message);
+        ObjectMapper mapper = new ObjectMapper();
+        String messageString = (String) message;
+        HashMap<String, UUID> messageMap = mapper.readValue(messageString, HashMap.class);
+        eventService.reserveEventTicket(messageMap.get("eventId"),
+                messageMap.get("ticketId"),
+                messageMap.get("userId").toString());
+        LOGGER.info("Ticket reserved successfully for eventId={}, ticketId={}, userId={}",
+                messageMap.get("eventId"),
+                messageMap.get("ticketId"),
+                messageMap.get("userId"));
+        messageProducer.sendMessage("ticket-reserved", new HashMap<>(
+                Map.of("bookingId", messageMap.get("bookingId")
+                )));
     }
 
 }
