e94b46c07f49300aca4b97cf5d0e247596b01ddb
OmarMUhammed03 <omarb2515@gmail.com>
2025-06-15 03:44:09 +0300

    Add basic context load test for UserService application

---
+++ user-service/src/test/java/org/example/userservice/service/UserServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package org.example.userservice.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
